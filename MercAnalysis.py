0eb5214a3bc5b2e1ed9616d87b632c846ac9fdc9
Mateusz Grzybowski <mateusz.f.grzybowski@gmail.com>
2021-03-09 19:55:22 +0000

    Preprocessing update
    Updated with fully working data preprocessing

--- MercAnalysis.py
+++ MercAnalysis.py
@@ -1,8 +1,8 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
 df = pd.read_csv("merc.csv")
 
+# Checking the data
 #info = df.info()
 engines = df.engineSize.value_counts()  # 12 engines of size 0.0
 transmission = df.transmission.value_counts()  # everything seems to be normal
@@ -11,3 +11,18 @@ mileage = df.mileage.value_counts()  # good
 fuel = df.fuelType.value_counts()  # good
 tax = df.tax.value_counts()  # good, car tax can be equal to 0
 mpg = df.mpg.value_counts()  # good
+
+
+def preprocess_merc(df):
+    errors = df[df["engineSize"] == 0.0].index
+    for index in errors:
+        df = df.drop(index, axis=0)
+
+    old_years = df[df["year"] < 2000].index
+    for index in old_years:
+        df = df.drop(index, axis=0)
+
+    df = df.reset_index(drop=True)
+
+    return df
+
--- RegressionMerc.py
+++ RegressionMerc.py
@@ -2,41 +2,41 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.metrics import accuracy_score
+from MercAnalysis import preprocess_merc
+
 
 df = pd.read_csv("merc.csv")
-df['transmission'] = df['transmission'].replace(['Automatic', 'Semi-Auto', 'Manual', 'Other'], [3, 2, 1, 0])
-df['fuelType'] = df['fuelType'].replace(['Other', 'Petrol', 'Diesel', 'Hybrid'], [0, 1, 2, 3])
 y = df['price'].values
 
+# Preprocessing of the data
+le = LabelEncoder() # Using LabelEncoder to transform string attributes to integers
+transmission_le = le.fit_transform(df["transmission"].values)
+df["transmission"] = transmission_le
+fuel_le = le.fit_transform(df["fuelType"].values)
+df["fuelType"] = fuel_le
+model_le = le.fit_transform(df["model"].values)
+df["model"] = model_le
 
+data = preprocess_merc(df)
 
-
-#seek = {}
-#for model in df.iloc[:, 0]:
-    #if model not in seek:
-    #    seek[model] = 1
-   # else:
-     #   seek[model] += 1
-
-#models = {key: val for key, val in sorted(seek.items(), key=lambda item: item[1])}
-
-#for index, model in enumerate(seek):
-    #df["model"] = df["model"].replace(model, index)
-
+# Checking for correlations between price and other attributes
 corr_matrix = []
-for i in range(1, 9):
+for i in range(9):
     r = np.corrcoef(df.iloc[:, i], y)
     corr_matrix.append(r[0, 1])
 
+# Plotting the results
 cols = [col for col in df]
 
-#plt.bar(cols, corr_matrix)
-#plt.ylabel("Correlation coefficient")
-#plt.ylim(bottom=-1)
-#plt.xlabel("Attributes", labelpad=15)
-#plt.show()
+plt.bar(cols, corr_matrix)
+plt.ylabel("Correlation coefficient")
+plt.xlim(top=1)
+plt.ylim(bottom=-1)
+plt.xlabel("Attributes", labelpad=15)
+plt.show()
 
-# Read about sklearn.preprocessing.LinearEncoder
-# Check data for some non-sense data (ex. engineSize = 0.0)
 
 
